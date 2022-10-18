50cfff37a250c365a5f30097d07cbb6b1b826800
Joe P Shoulak <joe@squarecompass.io>
2022-10-17 21:01:05 -0500

    Beginning of User implementation

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Make our grid before adding blocks
-const grid = new Grid();
+const gameWindow = new Window();
 
-// Add 3 rows of blocks to the grid
-Block.makeRows(3).forEach(block => grid.addBlock(block));
+// Add 3 rows of blocks to the window
+Block.makeRows(3).forEach(block => gameWindow.addBlock(block));
+
+gameWindow.addUser(new User());
