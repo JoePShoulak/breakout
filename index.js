86df981f7d50924b4b6e9494ea34c68bd05ecaa5
Joe P Shoulak <joe@squarecompass.io>
2022-10-17 22:27:14 -0500

    beginning ball

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const gameWindow = new Window();
 Block.makeRows(3).forEach(block => gameWindow.addBlock(block));
 
 const user = new User(230, 10);
+const ball = new Ball(50, 50);
 
 gameWindow.addUser(user);
+gameWindow.addBall(ball);
 
 // TODO: Find out why I coudln't make this work in jQuery syntax
 document.addEventListener('keydown', (event) => {
-    user.move(event, gameWindow);
+    user.move(event);
 })
