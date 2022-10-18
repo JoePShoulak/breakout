cdd7e958ff3d71ae8eab5b3d2df8885c84e045ee
Joe P Shoulak <joe@squarecompass.io>
2022-10-17 21:22:41 -0500

    Created GameElement parent class to inherit out css formatting functions

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ const gameWindow = new Window();
 // Add 3 rows of blocks to the window
 Block.makeRows(3).forEach(block => gameWindow.addBlock(block));
 
-gameWindow.addUser(new User());
+gameWindow.addUser(new User(230, 10));
+
