b2fc17ad1b20f77b91989669ff3648050c63e6e5
Joe P Shoulak <joe@squarecompass.io>
2022-10-17 22:44:29 -0500

    major lucky find, major refactor

--- index.js
+++ index.js
@@ -10,7 +10,36 @@ const ball = new Ball(50, 50);
 gameWindow.addUser(user);
 gameWindow.addBall(ball);
 
-// TODO: Find out why I coudln't make this work in jQuery syntax
-document.addEventListener('keydown', (event) => {
-    user.move(event);
-})
+// // TODO: Find out why I coudln't make this work in jQuery syntax
+// document.addEventListener('keydown', (event) => {
+//     user.move(event);
+// })
+
+var keyState = {};    
+// FIXME: Got it from here https://stackoverflow.com/questions/12273451/how-to-fix-delay-in-javascript-keydown
+window.addEventListener('keydown',function(e){
+    keyState[e.keyCode || e.which] = true;
+},true);    
+window.addEventListener('keyup',function(e){
+    keyState[e.keyCode || e.which] = false;
+},true);
+
+let direction;
+
+function gameLoop() {
+    if (keyState[37] || keyState[65]){
+        direction = "left";
+    } else if (keyState[39] || keyState[68]){
+        direction = "right";
+    } else {
+        direction = null;
+    }
+
+    user.move(direction)
+
+    // redraw/reposition your object here
+    // also redraw/animate any objects not controlled by the user
+
+    setTimeout(gameLoop, 10);
+}    
+gameLoop();
