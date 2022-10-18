ba2f43ccc2a4d1bee58b4363dcab72d1e18ec81e
Joe P Shoulak <joe@squarecompass.io>
2022-10-17 20:16:10 -0500

    defined basic classes, implemented rendered bricks in grid

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+// Make our grid before adding blocks
+const grid = new Grid();
 
+// Define our blocks
+const blocks = [
+    new Block(10, 270),
+    new Block(120, 270),
+    new Block(230, 270),
+    new Block(340, 270),
+];
 
+// Add our blocks to the grid
+blocks.forEach(block => {
+    grid.addBlock(block);
+});
