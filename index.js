cff339a53d921b138ef9ca0a2f63d21321262187
Joe P Shoulak <joe@squarecompass.io>
2022-10-17 21:41:41 -0500

    major refactor

--- index.js
+++ index.js
@@ -4,5 +4,9 @@ const gameWindow = new Window();
 // Add 3 rows of blocks to the window
 Block.makeRows(3).forEach(block => gameWindow.addBlock(block));
 
-gameWindow.addUser(new User(230, 10));
+const user = new User(230, 10);
 
+gameWindow.addUser(user);
+
+// TODO: Find out why I coudln't make this work in jQuery syntax
+document.addEventListener('keydown', user.move)
